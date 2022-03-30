7b1148fb3eccff07faa010a9dabdc24a1e7c793a
Tarek Seba <tarek.seba@outlook.fr>
2022-03-30 09:32:50 +0200

    changed port

--- src/main/java/fr/univrouen/rss22/Rss22Sb1Application.java
+++ src/main/java/fr/univrouen/rss22/Rss22Sb1Application.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Rss22Sb1Application {
 
 	public static void main(String[] args) {
-		System.getProperties().put("server.port", 3000);
 		SpringApplication.run(Rss22Sb1Application.class, args);
 	}
 
